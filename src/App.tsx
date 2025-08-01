52233ccfe5a64a5607c54fbca6daf0b3ba1098d1
Kinglewey2nd <lwclark92@gmail.com>
2025-08-01 15:53:40 -0600

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -1,1 +1,21 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import MainPage from "./components/MainPage";
+import AuthPage from "./components/AuthPage";
+import ShopPage from "./components/ShopPage";
+import CollectionPage from "./components/CollectionPage";
 
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+        <Route path="/login" element={<AuthPage />} />
+        <Route path="/shop" element={<ShopPage />} />
+        <Route path="/collection" element={<CollectionPage />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
--- src/firebase.ts
+++ src/firebase.ts
@@ -1,1 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
+const firebaseConfig = {
+  apiKey: "AIzaSyDOMBhE3JTlh3fLEC98nXy0YWtsc2cpFAE",
+  authDomain: "spellgrave-f2e30.firebaseapp.com",
+  projectId: "spellgrave-f2e30",
+  storageBucket: "spellgrave-f2e30.appspot.com",
+  messagingSenderId: "387513452186",
+  appId: "1:387513452186:web:a049bac189f315b4088123",
+  measurementId: "G-FKXDG97B9Y"
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
