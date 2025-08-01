bab94e4360990620f4205e85f3f2d825b1c631e3
Kinglewey2nd <lwclark92@gmail.com>
2025-08-01 16:20:29 -0600

    Update MainPage.tsx

--- src/components/MainPage.tsx
+++ src/components/MainPage.tsx
@@ -1,1 +1,17 @@
+import { signOut } from "firebase/auth";
+import { auth } from "../firebase";
+
+function handleLogout() {
+  signOut(auth).then(() => {
+    window.location.href = "/login";
+  });
+}
+
+// In your JSX:
+<button
+  onClick={handleLogout}
+  className="mt-4 px-4 py-2 bg-red-500 hover:bg-red-600 rounded text-white text-sm"
+>
+  Logout
+</button>
 
