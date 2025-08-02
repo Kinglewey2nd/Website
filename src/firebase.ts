307abe0ffb186a6452abdad2819a356c1ce99532
Kinglewey2nd <lwclark92@gmail.com>
2025-08-02 11:51:50 -0600

    Update firebase.ts

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,4 +1,3 @@
-
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
@@ -13,11 +12,7 @@ const firebaseConfig = {
   measurementId: "G-FKXDG97B9Y"
 };
 
-const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-const db = getFirestore(app);
-const provider = new GoogleAuthProvider();
-
-export { auth, db, provider };
-
-export const googleProvider = new GoogleAuthProvider();
+export const app = initializeApp(firebaseConfig); // ✅ This was missing
+export const db = getFirestore(app);
+export const auth = getAuth(app);
+export const googleProvider = new GoogleAuthP
