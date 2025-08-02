eeaad881af70d4c66df8bb24cfd908fa7cce97c9
Kinglewey2nd <lwclark92@gmail.com>
2025-08-02 10:57:26 -0600

    Create Collection.tsx

---
+++ src/components/Collection.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Collection: React.FC = () => {
+  return (
+    <div className="p-6 text-white">
+      <h1 className="text-3xl font-bold mb-4">Your Collection</h1>
+      <p>This is where your collected cards will be displayed.</p>
+    </div>
+  );
+};
+
+export default Collection;
