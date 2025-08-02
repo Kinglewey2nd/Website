a0949a1c41b014e43e8cd2d668ec9d038b0aebd3
Kinglewey2nd <lwclark92@gmail.com>
2025-08-02 12:51:03 -0600

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,9 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
 import './index.css';
 
+
+console.log("✅ Main menu mounted");
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Router>
@@ -11,3 +14,4 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
     </Router>
   </React.StrictMode>
 );
+
