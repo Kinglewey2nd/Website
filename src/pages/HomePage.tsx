0ba7120b5908567ab2dd2b3e324c783ded821292
Kinglewey2nd <lwclark92@gmail.com>
2025-08-01 20:10:18 -0600

    q
    q

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,12 @@
+import '../styles/home.css';
+
+export default function HomePage() {
+  return (
+    <div className="home-background">
+      <div className="overlay">
+        <h1 className="title">Welcome to SpellGrave</h1>
+        <p className="subtitle">Collect. Battle. Trade.</p>
+      </div>
+    </div>
+  );
+}
