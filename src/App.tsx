fc2d7b887fba034d36b5b4727bdda9a765d21566
Kinglewey2nd <lwclark92@gmail.com>
2025-08-02 10:37:47 -0600

    1
    1

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,26 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import MainMenu from './components/MainMenu';
-import Login from './components/Login';
-import Signup from './components/Signup';
-import ResetPassword from './components/ResetPassword';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import MainMenu from "./components/MainMenu";
+import Login from "./components/Login";
+import Signup from "./components/Signup";
+import ForgotPassword from "./components/ForgotPassword";
+import Collection from "./components/Collection";
+import PackOpen from "./components/PackOpen";
 
-function App() {
+const App = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<MainMenu />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
-        <Route path="/reset-password" element={<ResetPassword />} />
-        <Route path="*" element={<div className="auth-page">Page Not Found</div>} />
+        <Route path="/forgot-password" element={<ForgotPassword />} />
+        <Route path="/collection" element={<Collection />} />
+        <Route path="/pack/open" element={<PackOpen />} />
+        <Route path="*" element={<div>Page not found</div>} />
       </Routes>
     </Router>
   );
-}
+};
 
 export default App;
