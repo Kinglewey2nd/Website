d8633d589ba9f4557dabe6d8f2d521361931c8a2
Kinglewey2nd <lwclark92@gmail.com>
2025-08-02 10:30:23 -0600

    1
    1

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,6 +1,9 @@
+// src/firebase.ts
+
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDOMBhE3JTlh3fLEC98nXy0YWtsc2cpFAE",
@@ -9,11 +12,14 @@ const firebaseConfig = {
   storageBucket: "spellgrave-f2e30.appspot.com",
   messagingSenderId: "387513452186",
   appId: "1:387513452186:web:a049bac189f315b4088123",
-  measurementId: "G-FKXDG97B9Y"
+  measurementId: "G-FKXDG97B9Y",
 };
 
-const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-const db = getFirestore(app);
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
 
-export { auth, db };
+// Firebase Features
+export const auth = getAuth(app);
+export const provider = new GoogleAuthProvider();
+export const db = getFirestore(app);
+export const storage = getStorage(app);
