c8eae2987cafe5cfbbb55255cd22c7ebc2d0dfd1
Kinglewey2nd <lwclark92@gmail.com>
2025-08-01 18:52:20 -0600

    2

--- src/main.tsx
+++ src/main.tsx
@@ -1,21 +1,10 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import App from './App';
-import PackPage from './pages/PackPage';
-import PackOpening from './components/PackOpening';
-import LoginPage from './pages/LoginPage';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/pack" element={<PackPage />} />
-        <Route path="/pack/open" element={<PackOpening />} />
-        <Route path="/login" element={<LoginPage />} />
-      </Routes>
-    </BrowserRouter>
+    <App />
   </React.StrictMode>
 );
