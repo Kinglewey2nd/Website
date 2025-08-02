ac81f1aedd55d0c99d821a016055de83c41231ca
Kinglewey2nd <lwclark92@gmail.com>
2025-08-02 15:05:35 -0600

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
-import { AuthProvider } from './contexts/AuthContext'; // Adjust path if needed
+import { AuthProvider } from './src/useAuth'; // Adjust path if needed
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
