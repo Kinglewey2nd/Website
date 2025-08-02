1019cd3a86830bb89a0d92552e0a7e7f21e2f814
Kinglewey2nd <lwclark92@gmail.com>
2025-08-02 15:12:42 -0600

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
-import { AuthProvider } from './src/useAuth';
+import { useAuth } from './useAuth';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
