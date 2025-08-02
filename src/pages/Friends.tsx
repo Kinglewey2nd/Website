02b619310fbeae987f76f873a6a9b8bab46e7c48
Kinglewey2nd <lwclark92@gmail.com>
2025-08-02 09:00:02 -0600

    Create Friends.tsx

---
+++ src/pages/Friends.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Header from '../components/Header';
+
+export default function Friends() {
+  return (
+    <>
+      <Header />
+    <div style={{ color: 'white', textAlign: 'center', paddingTop: '10vh' }}>
+      <h1>Friends Page (Coming Soon)</h1>
+    </div>
+    </>
+  );
